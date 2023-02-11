8128761ef0e590b5fac8ed7f3e7ade9b9c76eb9b
ojha-me <dineshojha1013@gmail.com>
2023-02-11 22:51:11 +0545

    Final product

---
+++ DineshPortfolio/Helper/Truncate.cs
@@ -0,0 +1,12 @@
+﻿namespace DineshPortfolio.Helper
+{
+    public class Truncate
+    {
+        public string TruncateDescription(string description, int length)
+        {
+            if (description.Length <= length)
+                return description;
+            return description.Substring(0, length) + "...";
+        }
+    }
+}
